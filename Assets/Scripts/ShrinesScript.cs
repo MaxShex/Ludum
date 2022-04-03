a492f8203ea598c99d1117dc3c6495da2bdc4f84
ikvelibrium <stalker.alex.perel@mail.ru>
2022-04-03 12:29:26 +0300

    ShrineMechanics

--- Assets/Scripts/ShrinesScript.cs
+++ Assets/Scripts/ShrinesScript.cs
@@ -8,17 +8,63 @@ public class ShrinesScript : MonoBehaviour
     public picker pickerScript;
     public timerScript timer;
     public float shrineAddsTime;
+    private Rigidbody2D rb;
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private GameObject tribute;
+    
+    public bool spawn = true;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "shrine")
+        if (collision.gameObject.tag == "shrine" && spawn == true)
         {
-            if (Input.GetKeyDown(KeyCode.F) && timer.timeLeft >= timer.maxTime - shrineAddsTime && pickerScript.holdChecker)
+            if ( timer.timeLeft >= timer.maxTime - shrineAddsTime )
             {
-                timer.timeLeft = timer.maxTime - timer.timeLeft;
+                timer.maxTime = timer.maxTime - 3;
+                timer.timeLeft += timer.maxTime - timer.timeLeft;
+
+                Instantiate(tribute, spawnPoint.position, Quaternion.Euler(new Vector3(0, 0, 0)));
+                Destroy(gameObject, 0.65f);
                 
-            } else if (Input.GetKeyDown(KeyCode.F) && timer.timeLeft <= timer.maxTime - shrineAddsTime && pickerScript.holdChecker)
+                spawn = false;
+
+            } else if ( timer.timeLeft <= timer.maxTime - shrineAddsTime  )
             {
-                timer.timeLeft += 5f;
+                timer.maxTime = timer.maxTime - 3;
+                timer.timeLeft += shrineAddsTime;
+                Instantiate(tribute, spawnPoint.position, Quaternion.Euler(new Vector3(0, 0, 0)));
+                Destroy(gameObject, 0.65f);
+
+                spawn = false;
+
+            }
+        }
+        if (collision.gameObject.tag == "SecondShrine" && spawn == false)
+        {
+            if (timer.timeLeft >= timer.maxTime - shrineAddsTime )
+            {
+                timer.maxTime = timer.maxTime - 3;
+                timer.timeLeft += timer.maxTime - timer.timeLeft;
+
+                Instantiate(tribute, spawnPoint.position, Quaternion.Euler(new Vector3(0, 0, 0)));
+                Destroy(gameObject);
+                spawn = true;
+
+            }
+            else if (timer.timeLeft <= timer.maxTime - shrineAddsTime )
+            {
+                timer.maxTime = timer.maxTime - 3;
+                timer.timeLeft += shrineAddsTime;
+                Instantiate(tribute, spawnPoint.position, Quaternion.Euler(new Vector3(0, 0, 0)));
+                Destroy(gameObject);
+                spawn = true;
+
             }
         }
     }
