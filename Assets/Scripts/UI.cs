54da6a27c64021b4f59b36deb00a6149215c01b5
Alexander Baydalinov <64281569+Rezenkron@users.noreply.github.com>
2022-04-03 14:28:53 +0300

    UI added

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UI : MonoBehaviour
+{
+    [SerializeField] private GameObject PausePanel;
+    int currentScene;
+    public void PauseButton()
+    {
+        Time.timeScale = 0;
+        PausePanel.SetActive(true);
+    }
+    public void ContinueButton()
+    {
+        Time.timeScale = 1;
+        PausePanel.SetActive(false);
+    }
+    
+    public void RestartButton()
+    {
+        currentScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentScene);
+        Time.timeScale = 1;
+    }
+}
